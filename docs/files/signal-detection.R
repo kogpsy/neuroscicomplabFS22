6118edad726eae3e3ff4c1015e5d4c79ae0280a6
Andrew Ellis <andrewellis@pm.me>
2022-03-22 09:38:38 +0100

    Rebuild for session 5

---
+++ docs/files/signal-detection.R
@@ -0,0 +1,259 @@
+
+
+# Simulate a signal detection experiment ----
+
+N <- 150
+pH <- 0.9
+pFA <- 0.1
+H <- rbinom(1, N, pH)
+
+M <- N - H
+FA <- rbinom(1, N, pFA)
+CR <- N - FA
+
+Responses <- as.table(matrix(c(H, M, FA, CR), nc = 2,
+                                     dimnames = list(Response = c("Yes", "No"),
+                                                     Signal = c("Present", "Absent"))))
+
+
+
+
+## write a function to simulate experiments ----
+dp <- 1
+crit <- 0.1
+nS <- nN <- 1000
+
+
+pFA <- 1 - pnorm(crit)
+pH <- 1 - pnorm(crit - dp)
+FA <- rbinom(1, nN, pFA)
+H <- rbinom(1, nS, pH)
+CR <- nN - FA
+M <- nS - H
+
+
+## function ----
+sim_sdt <- function(dp = 1, c = 0, N = 1000) {
+
+    nS <- nN <- N
+    pFA <- 1 - pnorm(-0.5*dp - c)
+    pH <- 1 - pnorm(0.5*dp - c)
+    FA <- rbinom(1,nN, pFA)
+    H <- rbinom(1, nS, pH)
+    CR <- nN-FA
+    M <- nS-H
+res <- matrix(c(H, M, FA, CR),
+              nc = 2,
+              dimnames = list(c("Yes", "No"),
+                              c("Signal", "No Signal")))
+as.table(res)
+}
+
+## call function with default arguments ----
+sim_sdt()
+
+
+
+
+## ----echo=FALSE---------------------------------------------
+library(tidyverse)
+library(viridis)
+xlim <- c(-4.5, 4.5)
+alpha <- c(0.6, 0.2)
+
+dprime <- 1
+criterion <- -0.2
+colors <- viridis(n = 4, 
+                  begin = 0, 
+                  end = 0.98, 
+                  direction = -1)
+
+p1 <- tibble(x = seq(xlim[1], xlim[2], by = 0.1)) |> 
+    ggplot(aes(x)) +
+    stat_function(fun = dnorm, colour = colors[1], 
+                  args = list(mean = -dprime/2, sd = 1),
+                  size = 1.5) +
+    stat_function(fun = dnorm, colour = colors[4], 
+                  args = list(mean = dprime/2, sd = 1),
+                  size = 1.5) +
+    geom_vline(xintercept = c(-dprime/2, dprime/2), size = 1, linetype = "dotted", 
+               alpha =  0.4) +
+    scale_y_continuous(breaks = NULL) +
+    scale_x_continuous(labels = NULL) +
+    labs(x = "Familarity", y = "") +
+    annotate("text", 
+           x = 0.1, 
+           y = dnorm(dprime/2, mean = dprime/2, sd = 1) + 0.03,
+           label = "d'", 
+           size = 8) +
+    annotate("segment", x = -dprime/2, 
+                 xend = dprime/2, 
+                 y = dnorm(dprime/2, mean = dprime/2, sd = 1) + 0.01, 
+                 yend = dnorm(dprime/2, mean = dprime/2, sd = 1) + 0.01,
+           size = 1) +
+    annotate("text", 
+           x = -1.5, 
+           y = dnorm(dprime/2, mean = dprime/2, sd = 1) + 0.03,
+           label = "new", 
+           size = 6, 
+           color = "grey60") +
+      annotate("text", 
+           x = 1.5, 
+           y = dnorm(dprime/2, mean = dprime/2, sd = 1) + 0.03,
+           label = "old", 
+           size = 6, 
+           color = "grey60") +
+  theme_linedraw()
+p1
+
+
+## ----echo=FALSE---------------------------------------------
+p2 <- p1 + 
+  geom_vline(xintercept = 0, size = 1, 
+               alpha = 0.4,
+             linetype = "dashed") +
+  geom_area(stat = "function", fun = dnorm, 
+              args = list(mean = -dprime/2, sd = 1),
+              fill = colors[1], alpha = 0.6,
+              xlim = c(criterion, xlim[2])) +
+
+  geom_area(stat = "function", fun = dnorm, 
+              args = list(mean = dprime/2, sd = 1),
+              fill = colors[4], alpha = alpha[2],
+              xlim = c(criterion, xlim[2])) +
+  geom_vline(xintercept = criterion, size = 1, 
+               linetype = 1) +
+    annotate("text", 
+           x = -0.05,
+           y = -0.02,
+           label = "c", 
+           size = 8)
+p2
+
+
+## ----echo=FALSE---------------------------------------------
+tibble(x = seq(0, 1, by = 0.01)) |> 
+  ggplot(aes(x)) +
+  stat_function(fun = qnorm, colour = "steelblue3", 
+                  args = list(mean = 0, sd = 1),
+                  size = 1.5) +
+  labs(x = "Probability", y = "Score") +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(-3.5, 3.5), breaks = -3:3) + 
+  ggtitle("Probit function / quantile function / inverse cdf / qnorm()") +
+  theme_linedraw()
+
+
+## ----echo=FALSE---------------------------------------------
+xlim <- c(-4.5, 4.5)
+alpha <- c(0.6, 0.2)
+
+dprime <- 1
+criterion <- -0.2
+colors <- viridis(n = 4, 
+                  begin = 0, 
+                  end = 0.98, 
+                  direction = -1)
+
+tibble(x = seq(xlim[1], xlim[2], by = 0.1)) |> 
+    ggplot(aes(x)) +
+    stat_function(fun = dnorm, colour = colors[1], 
+                  args = list(mean = -dprime/2, sd = 1),
+                  size = 1.5) +
+    geom_area(stat = "function", fun = dnorm, 
+              args = list(mean = -dprime/2, sd = 1),
+              fill = colors[1], alpha = 0.6,
+              xlim = c(criterion, xlim[2])) +
+    geom_vline(xintercept = criterion, size = 1, 
+               linetype = 1) +
+  ggtitle("False Alarms") +
+  theme_linedraw()
+
+
+## ----echo=FALSE---------------------------------------------
+tibble(x = seq(xlim[1], xlim[2], by = 0.1)) |> 
+    ggplot(aes(x)) +
+    stat_function(fun = dnorm, colour = colors[4], 
+                  args = list(mean = dprime/2, sd = 1),
+                  size = 1.5) +
+    geom_area(stat = "function", fun = dnorm, 
+              args = list(mean = dprime/2, sd = 1),
+              fill = colors[4], alpha = 0.6,
+              xlim = c(criterion, xlim[2])) +
+    geom_vline(xintercept = criterion, size = 1, 
+               linetype = 1) +
+  ggtitle("Hits") +
+  theme_linedraw()
+
+
+## ----echo=FALSE---------------------------------------------
+p2
+
+
+## -----------------------------------------------------------
+# You might first need to install the `remotes` package
+# install.packages("remotes")
+# install sdtalt
+# remotes::install_github("cran/sdtalt")
+
+library(sdtalt)
+library(tidyverse)
+
+data(confcontr)
+
+confcontr <- as_tibble(confcontr) |> 
+  mutate(subno = as_factor(subno),
+         item = isold - 0.5)
+
+
+## -----------------------------------------------------------
+confcontr
+
+
+## -----------------------------------------------------------
+sdt <- confcontr |> 
+  mutate(type = case_when(
+        isold==1 & sayold==1 ~ "Hit",
+        isold==1 & sayold==0 ~ "Miss",
+        isold==0 & sayold==0 ~ "CR",
+        isold==0 & sayold==1 ~ "FA"))
+
+
+## -----------------------------------------------------------
+sdt_summary <- sdt |>
+    group_by(subno) |>
+    count(type) |> 
+  pivot_wider(names_from = type, values_from = n) 
+
+
+## -----------------------------------------------------------
+correct_zero_count <- function(x) {
+    x = ifelse(is.na(x), 0, x)
+    x = x + 0.001
+}
+
+sdt_summary <- sdt_summary |>
+    mutate(hit_rate = Hit/(Hit + Miss),
+           fa_rate = FA/(FA + CR),
+           zhr = qnorm(hit_rate),
+           zfa = qnorm(fa_rate),
+           dprime = zhr - zfa,
+           c = -0.5 * (zhr + zfa)) |>
+    mutate(across(c(dprime, c), round, 2))
+
+
+## -----------------------------------------------------------
+sdt_summary |> 
+  filter(subno == 53) |> 
+  select(subno, hit_rate, fa_rate, zhr, zfa, dprime, c)
+
+
+## -----------------------------------------------------------
+subno53 <- confcontr |> 
+  filter(subno == 53)
+
+fit_glm_53 <- glm(sayold ~ isold, 
+                  family = binomial(link = "probit"),
+                  data = subno53)
+summary(fit_glm_53)
+
--- docs/src/sim-sdt.R
+++ docs/src/sim-sdt.R
@@ -1,3 +1,6 @@
+
+# Simulate a signal detection experiment ----
+
 N <- 150
 pH <- 0.9
 pFA <- 0.1
@@ -14,6 +17,7 @@ Responses <- as.table(matrix(c(H, M, FA, CR), nc = 2,
 
 
 
+## write a function to simulate experiments ----
 dp <- 1
 crit <- 0.1
 nS <- nN <- 1000
@@ -21,13 +25,13 @@ nS <- nN <- 1000
 
 pFA <- 1 - pnorm(crit)
 pH <- 1-pnorm(crit-dp)
-FA <- rbinom(1,nN, pFA)
+FA <- rbinom(1, nN, pFA)
 H <- rbinom(1, nS, pH)
 CR <- nN-FA
 M <- nS-H
 
 
-
+## function ----
 sim_sdt <- function(dp = 1, c = 0, N = 1000) {
 
     nS <- nN <- N
@@ -46,48 +50,6 @@ res <- matrix(c(H, M, FA, CR),
 as.table(res)
 }
 
+## call function with default arguments ----
 sim_sdt()
 
-
-## -----
-library(sdtalt)
-data("confcontr")
-
-sdt <- confcontr %>%
-    mutate(
-        type = "hit",
-        type = ifelse(isold == 1 & sayold == 0, "miss", type),
-        type = ifelse(isold == 0 & sayold == 0, "cr", type), # Correct rejection
-        type = ifelse(isold == 0 & sayold == 1, "fa", type) # False alarm
-    )
-
-sdt <- sdt %>%
-    group_by(subno, type) %>%
-    summarise(count = n()) %>%
-    spread(type, count) # Format data to one row per person
-
-sdt <- sdt %>%
-    mutate(
-        zhr = qnorm(hit / (hit + miss)),
-        zfa = qnorm(fa / (fa + cr)),
-        dprime = zhr - zfa,
-        crit = -zfa
-    )
-
-
-# ```{r sdtplot-1, echo = F, fig.cap = "The equal variance Gaussian signal detection model for the first participant in the data, based on manual calculation of the parameter's point estimates. The two distributions are the noise distribution (dashed) and the signal distribution (solid); the dotted vertical line represents the response criterion. d' is the distance between the peaks of the two distributions."}
-# Basic plot
-p1 <- ggplot(data.frame(x = seq(-5, 6, by = .01)), aes(x = x)) +
-    coord_cartesian(ylim = c(0, 0.5), xlim = c(-3.5, 4), expand = 0) +
-    labs(x = "Signal strength", y = "Probability")
-# Add noise & signal distributions, and criterion
-p1 + stat_function(
-    fun = dnorm, geom = "line", lty = 2,
-    args = list(mean = 0, sd = 1)
-) +
-    stat_function(
-        fun = dnorm, geom = "line",
-        args = list(mean = sdt$dprime[1], sd = 1)
-    ) +
-    geom_vline(xintercept = .5, lty = 3)
-# ```
